4e989361a5a8d69ecd8780c878746f3e48b56f38
Brionna <brimorris317@gmail.com>
2024-04-07 10:46:54 -0400

    Updated Tutorial Files

---
+++ files.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+// reading files
+//fs.readFile('./blog1.txt', (err, data) => {
+    //if(err) {
+        //console.log(err);
+    //}
+    //console.log(data.toString());
+//});
+
+//console.log('last line');
+
+
+//writing files
+//fs.writeFile('./blog1.txt', 'hello, world', () => {
+  //  console.log('file was written');
+//});
+
+//directories
+if (!fs.existsSync('./assets')) {
+
+fs.mkdir('./assets', (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('folder created'); 
+}); 
+} else {
+   fs.rmdir('./assets', (err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log('folder deleted');
+   });
+}
+
+
+
+// deleting files
+if (fs.existsSync('./deleteme.txt')) {
+    fs.unlink('./deleteme.txt', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('file deleted');
+    })
+}
